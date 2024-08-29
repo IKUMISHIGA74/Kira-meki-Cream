09414cf269d8da147e908161cf38d224adb68573
IKUMISHIGA74 <uminomori74@gmail.com>
2024-08-30 07:04:18 +0900

    初稿納品

--- assets/script/main.js
+++ assets/script/main.js
@@ -1,10 +1,53 @@
-$(document).on('ready', function() {
-    if (window.matchMedia("(max-width: 767px)").matches) {
+// スライダー
+function initializeSlick() {
+    if ($('.voice__list').hasClass('slick-initialized')) {
+        $('.voice__list').slick('unslick');
+    }
+
+    // SP仕様 
+    if (window.matchMedia('(max-width: 1279px)').matches) {
         $(".voice__list").slick({
             dots: true,
             infinite: true,
+            arrows: true,
             slidesToShow: 1,
-            slidesToScroll: 1
+            slidesToScroll: 1,
+            prevArrow: '<div class="slide-arrow arrow__prev"><img src="assets/image/arrow_01.png" alt=""></div>',
+            nextArrow: '<div class="slide-arrow arrow__next"><img src="assets/image/arrow_02.png" alt=""></div>'
         });
     }
+    // PS仕様
+    else if (window.matchMedia('(min-width: 1280px)').matches) {
+        $(".voice__list").slick({
+            dots: true,
+            infinite: true,
+            arrows: true,
+            slidesToShow: 3,
+            slidesToScroll: 3,
+            prevArrow: '<div class="slide-arrow arrow__prev"><img src="assets/image/arrow_01.png" alt=""></div>',
+            nextArrow: '<div class="slide-arrow arrow__next"><img src="assets/image/arrow_02.png" alt=""></div>'
+        });
+    }
+}
+
+initializeSlick();
+
+$(window).on('resize', function() {
+    initializeSlick();
+});
+
+// アコーディオン
+$('.faq__list').each(function() {
+    var $faqSection = $(this);
+    $faqSection.find('.faq__list--question').click(function() {
+        $faqSection.find('.faq__list--question').not(this).next().slideUp();
+        $faqSection.find('.faq__list--question').not(this).removeClass('active').find('i').removeClass('fa-caret-down').addClass('fa-caret-up');
+        $(this).next().slideToggle();
+        $(this).toggleClass('active');
+        if ($(this).hasClass('active')) {
+            $(this).find('i').removeClass('fa-caret-up').addClass('fa-caret-down');
+        } else {
+            $(this).find('i').removeClass('fa-caret-down').addClass('fa-caret-up');
+        }
+    });
 });
