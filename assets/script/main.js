dfc4f363a44ce91694c16497c3b2e6128793f212
IKUMISHIGA74 <uminomori74@gmail.com>
2024-08-30 07:18:20 +0900

    初稿納品

--- assets/script/main.js
+++ assets/script/main.js
@@ -1,4 +1,3 @@
-// スライダー
 function initializeSlick() {
     if ($('.voice__list').hasClass('slick-initialized')) {
         $('.voice__list').slick('unslick');
@@ -10,13 +9,25 @@ function initializeSlick() {
             dots: true,
             infinite: true,
             arrows: true,
+            swipe: true,
             slidesToShow: 1,
             slidesToScroll: 1,
             prevArrow: '<div class="slide-arrow arrow__prev"><img src="assets/image/arrow_01.png" alt=""></div>',
-            nextArrow: '<div class="slide-arrow arrow__next"><img src="assets/image/arrow_02.png" alt=""></div>'
+            nextArrow: '<div class="slide-arrow arrow__next"><img src="assets/image/arrow_02.png" alt=""></div>',
+            touchThreshold: 10,
+            speed: 300, 
+            responsive: [
+                {
+                    breakpoint: 768,
+                    settings: {
+                        slidesToShow: 1,
+                        slidesToScroll: 1,
+                    }
+                }
+            ]
         });
     }
-    // PS仕様
+    // PC仕様
     else if (window.matchMedia('(min-width: 1280px)').matches) {
         $(".voice__list").slick({
             dots: true,
@@ -36,9 +47,11 @@ $(window).on('resize', function() {
     initializeSlick();
 });
 
+
 // アコーディオン
 $('.faq__list').each(function() {
     var $faqSection = $(this);
+    $faqSection.find('.faq__list--question').next().hide();
     $faqSection.find('.faq__list--question').click(function() {
         $faqSection.find('.faq__list--question').not(this).next().slideUp();
         $faqSection.find('.faq__list--question').not(this).removeClass('active').find('i').removeClass('fa-caret-down').addClass('fa-caret-up');
